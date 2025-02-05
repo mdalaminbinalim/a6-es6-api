0cac39ea7c4964d74f96f3fa997b835f03394a1d
mdalaminbinalim <alamin29107@gmail.com>
2025-02-05 11:41:58 +0600

    added category functionalities

--- js/pet.js
+++ js/pet.js
@@ -14,24 +14,29 @@ const displayCategories = (categories) => {
     const categorySection = document.getElementById('category-container');
 
     categories.forEach((category) => {
-        const categoryButton = document.createElement('button');
-        categoryButton.classList = 'rounded-full font-bold md:p-4 p-3 hover:ring-2 hover:ring-[rgb(14,122,129)] hover:bg-[rgba(14,122,129,0.1)]';
+        const categoryDiv = document.createElement('div');
 
-        categoryButton.innerHTML = `
-        <div class="flex justify-evenly items-center"><img class="inline" src="${category.category_icon}">
-        <p class="font-bold text-xl md:text-2xl">${category.category}</p></div>
+        categoryDiv.innerHTML = `
+        <button onclick="loadPets(true, '${category.category}')" class="flex items-center justify-evenly gap-3 md:gap-5 rounded-full font-bold md:py-3 md:mx-0 px-8 py-2 md:px-16 hover:ring-2 hover:ring-[rgb(14,122,129)] hover:bg-[rgba(14,122,129,0.1)]"><img class="inline" src="${category.category_icon}"><p class="font-bold text-xl md:text-2xl">${category.category}</p></button>
+        <div>
+        </div>
         `;
 
-        categorySection.append(categoryButton);
+        categorySection.append(categoryDiv);
     })
 }
 
 // All Pets data fetched
-const loadPets = async () => {
+const loadPets = async (status, petType) => {
     try {
-        const response = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`);
+        const response = await fetch(`https://openapi.programming-hero.com/api/peddy/${status ? `category/${petType}` : 'pets'}`);
         const data = await response.json();
-        displayPets(data.pets);
+        if (status) {
+            displayPets(data.data);
+        }
+        else {
+            displayPets(data.pets);
+        }
     } catch (error) {
         console.error("ERROR in loading data", error);
     }
@@ -39,21 +44,22 @@ const loadPets = async () => {
 
 // Display all pets
 const displayPets = (pets) => {
+    document.getElementById('pet-details').innerHTML = '';
     const petsContainer = document.getElementById('all-pets-container');
-    petsContainer.classList = 'grid md:grid-cols-4 grid-cols-1 md:my-8 my-6 gap-6';
+    const petDetails = document.getElementById('pet-details');
 
     pets.forEach((pet) => {
         const petDiv = document.createElement('div');
         const { petId, breed, category, birth, price, image, gender } = pet;
 
         petDiv.innerHTML = `
-        <div class="p-6 border-2 shadow-md rounded-xl space-y-2 md:h-[449px]">
-                <img src="${image}" class="rounded-xl">
+        <div class="p-6 border-2 shadow-xl md:shadow-md rounded-xl space-y-2 md:h-[432px]">
+                <img src="${image}" class="rounded-xl w-[272px] h-[160px]">
                 <h2 class="text-xl font-extrabold">${pet.pet_name}</h2>
                 <p class="basic font-bold"><img class="inline w-5" src="images/icons8-apps-48.png" alt=""> Breed: ${breed}</p>
                 <p class="basic font-bold"><img class="inline w-5" src="images/icons8-date-50.png" alt=""> Birth: ${pet.date_of_birth}</p>
                 <p class="basic font-bold"><img class="inline w-5" src="images/icons8-male-female-32.png" alt=""> Gender: ${gender}</p>
-                <p class="basic font-bold"><img class="inline w-5" src="images/icons8-dollar-50.png" alt=""> Price: ${price}</p>
+                <p class="basic font-bold"><img class="inline w-5" src="images/icons8-dollar-50.png" alt=""> Price: $${price}</p>
                 <div class="flex items-center gap-5 pt-4">
                     <div><img class="inline w-5 cursor-pointer" src="images/icons8-like-50.png" alt=""></div>
                     <div><button class="basic hover:ring-2 hover:ring-[rgb(14,122,129)] hover:bg-[rgba(14,122,129,0.1)] font-semibold text-lg rounded-xl md:py-2 py-1 md:px-5 px-3">Adopt</button></div>
@@ -62,11 +68,13 @@ const displayPets = (pets) => {
             </div>
         `;
 
-        petsContainer.append(petDiv);
+        petDetails.append(petDiv);
     })
+    petsContainer.append(petDetails);
 }
+
 
 // Category Section's function called
 loadCategories();
 // All pets function called
-loadPets();
+loadPets(false, 'pets');
