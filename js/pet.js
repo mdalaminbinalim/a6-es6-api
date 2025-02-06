fa31e02dc23a79a6ceb9f731e481b6550357a0c0
mdalaminbinalim <alamin29107@gmail.com>
2025-02-06 10:36:26 +0600

    added active button function

--- js/pet.js
+++ js/pet.js
@@ -17,14 +17,28 @@ const displayCategories = (categories) => {
         const categoryDiv = document.createElement('div');
 
         categoryDiv.innerHTML = `
-        <button onclick="loadPets(true, '${category.category}')" class="flex items-center justify-evenly gap-3 md:gap-5 rounded-full font-bold md:py-3 md:mx-0 px-8 py-2 md:px-16 hover:ring-2 hover:ring-[rgb(14,122,129)] hover:bg-[rgba(14,122,129,0.1)]"><img class="inline" src="${category.category_icon}"><p class="font-bold text-xl md:text-2xl">${category.category}</p></button>
+        <button onclick="loadPets(true, '${category.category}')" class="category-btn active-btn flex items-center justify-evenly gap-3 md:gap-5 rounded-full font-bold md:py-3 md:mx-0 px-8 py-2 md:px-16 hover:ring-2 hover:ring-[rgb(14,122,129)] hover:bg-[rgba(14,122,129,0.1)]"><img class="inline" src="${category.category_icon}"><p class="font-bold text-xl md:text-2xl">${category.category}</p></button>
         <div>
         </div>
         `;
 
         categorySection.append(categoryDiv);
-    })
+    });
+
+    // Active Button
+    const buttons = document.querySelectorAll('.category-btn');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            buttons.forEach(btn => {
+                btn.style.backgroundColor = '';
+                btn.style.boxShadow = '';
+            });
+            button.style.backgroundColor = 'rgba(14,122,129,0.1)';
+            button.style.boxShadow = '0 0 0 2px rgb(14,122,129)';
+        });
+    });
 }
+
 
 // All Pets data fetched
 const loadPets = async (status, petType) => {
@@ -44,16 +58,25 @@ const loadPets = async (status, petType) => {
 
 // Display all pets
 const displayPets = (pets) => {
+    const spinner = document.getElementById('spinner');
+    const likedContainer = document.getElementById('liked-pets');
+    spinner.style.display = 'block';
+    likedContainer.classList.add('hidden');
     document.getElementById('pet-details').innerHTML = '';
-    const petsContainer = document.getElementById('all-pets-container');
-    const petDetails = document.getElementById('pet-details');
 
-    if (pets.length !== 0) {
-        pets.forEach((pet) => {
-            const petDiv = document.createElement('div');
-            const { petId, breed, category, birth, price, image, gender } = pet;
+    setTimeout(function () {
+        spinner.style.display = 'none';
+        likedContainer.classList.remove('hidden');
+        document.getElementById('pet-details').innerHTML = '';
+        const petsContainer = document.getElementById('all-pets-container');
+        const petDetails = document.getElementById('pet-details');
 
-            petDiv.innerHTML = `
+        if (pets.length !== 0) {
+            pets.forEach((pet) => {
+                const petDiv = document.createElement('div');
+                const { petId, breed, category, birth, price, image, gender } = pet;
+
+                petDiv.innerHTML = `
         <div class="p-6 border-2 shadow-xl md:shadow-md rounded-xl space-y-2 md:h-[432px]">
                 <img src="${image}" class="rounded-xl w-[272px] h-[160px]">
                 <h2 class="text-xl font-extrabold">${pet.pet_name ? pet.pet_name : 'Not Named Yet'}</h2>
@@ -69,23 +92,26 @@ const displayPets = (pets) => {
             </div>
         `;
 
-            petDetails.append(petDiv);
-        })
-        petsContainer.append(petDetails);
-        petDetails.classList = "grid md:grid-cols-3 grid-cols-1 md:my-8 my-6 gap-6 mx-auto";
-    }
-    else {
-        petDetails.classList = "items-center grid grid-cols-1";
-        petDetails.innerHTML = `
-            <div class="flex flex-col justify-center items-center w-3/4 md:my-8 my-6 gap-6 mx-auto">
+                petDetails.append(petDiv);
+            })
+            petsContainer.append(petDetails);
+            petDetails.classList = "grid md:grid-cols-3 grid-cols-1 md:my-8 my-6 gap-6 mx-auto";
+        }
+        else {
+            petDetails.classList = "items-center grid grid-cols-1";
+            petDetails.innerHTML = `
+            <div class="flex flex-col justify-center items-center md:w-3/4 md:my-8 my-6 gap-6 mx-auto">
                     <img src="./images/error.webp">
                     <h1 class="text-3xl font-extrabold text-center">No Information Available</h1>
                     <p class="basic font-bold text-center">As the rate of selling birds was comparatively higher last month, it went out of stock. We are currently unable to provide you your desired pet. Keep in touch with us for further information.</p>
             </div>
         `;
-        petsContainer.append(petDetails);
-    }
+            petsContainer.append(petDetails);
+        }
+    }, 2000);
+
 }
+
 
 // Liked pets Function
 const petById = async (petID) => {
